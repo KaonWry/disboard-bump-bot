ea430e0664376f6b7cfeecc972472e2ef8a7f268
KaonWry <titan2011official@gmail.com>
2022-03-26 22:20:57 +0700

    Cant work anything out smh

--- index.js
+++ index.js
@@ -1,6 +1,26 @@
-var discord = require("discord.js");
-var client = new discord.Client();
+const { Client, Intents } = require('discord.js');
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
+require('dotenv').config();
+
+
 
 client.on("ready", () => {
+	console.log(client.user.tag +  " logged in.");
+});
 
+
+client.on("message", msg => {
+	if (msg.content === "help") {
+		msg.reply("shut up");
+	}
+
+	if (msg.content === "test") {
+		setInterval(function() {
+    		msg.channel.send("Poggers");
+    		console.log("pogger")
+		}, 10000);
+	}
 })
+
+
+client.login(process.env.TOKEN);
